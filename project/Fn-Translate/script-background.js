5dfecd15a45e17e129f29c45e9440ff567d83562
tmp20190820 <54312936+tmp20190820@users.noreply.github.com>
2020-03-06 17:27:25 +0900

    .

--- project/Fn-Translate/script-background.js
+++ project/Fn-Translate/script-background.js
@@ -1,28 +1,161 @@
-browser.contextMenus.create({ // 컨텍스트 메뉴 생성
+let responseJSONArray = []
+
+browser.menus.create({ // 컨텍스트 메뉴 생성
 	id: 'translate',
 	title: 'translate',
 	contexts: ['selection']
 })
-browser.contextMenus.create({
+browser.menus.create({
+	id: 'notification',
+	title: 'notification',
+	contexts: ['all']
+})
+browser.menus.create({
+	id: 'test1',
+	title: 'test1',
+	contexts: ['all']
+})
+browser.menus.create({
+	id: 'test2',
+	title: 'test2',
+	contexts: ['all']
+})
+browser.menus.create({
 	id: 'version',
 	title: 'version',
 	contexts: ['all']
 })
 
-browser.contextMenus.onClicked.addListener((info) => { // 컨텍스트 메뉴 동작
+browser.menus.onClicked.addListener((info, tab) => { // 컨텍스트 메뉴 동작
+	// console.log(info)
+	// console.log(tab)
+	// console.log(info.selectionText)
+	// console.log(tab.id)
 	if (info.menuItemId === 'translate') {
 		sendMessageTab('translate')
+	} else if (info.menuItemId === 'notification') {
+		let tmp1 = ''
+		for (let i1 = 1; i1 <= 20; i1++) {
+			let tmp2 = ''
+			for (let i2 = 0; i2 < 10 - String(i1).length; i2++) {
+				tmp2 += 'a'
+			}
+			tmp1 += tmp2 + i1
+		}
+		browser.notifications.create(
+			'notification id1',
+			{
+				type: 'basic',
+				title: tmp1,
+				message: tmp1 // 최대 200char
+			}
+		)
+	} else if (info.menuItemId === 'test1') {
+		translateRequest(info.selectionText)
+		// browser.notifications.create(
+		// 	'notification id1',
+		// 	{
+		// 		type: 'basic',
+		// 		title: 'title1',
+		// 		message: 'message1'
+		// 	}
+		// )
+	} else if (info.menuItemId === 'test2') {
+		console.log(tab)
 	} else if (info.menuItemId === 'version') {
-		let version = 'v20191223'
+		let version = 'v20200212'
 		browser.tabs.executeScript({ code: `alert('${version}')` })
 	}
 })
 
-async function sendMessageTab (string) { // 활성 탭으로 메시지 발신
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => { // 메시지 리스너
+	if (message.id === 'translate') {
+		console.log(message)
+		console.log(sender)
+		translateRequest(message.selectionText, () => {
+			sendResponse(responseJSONArray)
+		})
+		return true // 'return true' 비동기
+	}
+})
+
+async function sendMessageTab (string) { // 활성 탭으로 메시지
 	let activeTabArray = await browser.tabs.query({
 		active: true, currentWindow: true
 	})
 	let tabId = activeTabArray[0].id
 
-	await browser.tabs.sendMessage(tabId, [string, 0])
+	await browser.tabs.sendMessage(tabId, { id: string })
+}
+
+function tkkRequest (loadendFunction) { // tkk 요청
+	let xhr = new XMLHttpRequest()
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			// tkk:'439260.900540207'
+			setTkk(/\d+\.\d+/.exec(/tkk:'\d+\.\d+'/.exec(xhr.responseText)[0])[0])
+			console.log(getTkk())
+		}
+	}
+	xhr.open('GET', 'https://translate.google.com/', true)
+	xhr.send()
+	xhr.onloadend = () => {
+		if (getTkk()) {
+			if (loadendFunction) loadendFunction()
+		} else {
+			browser.notifications.create(
+				'notification id1',
+				{
+					type: 'basic',
+					title: 'translate',
+					message: 'tkk error'
+				}
+			)
+		}
+	}
+}
+
+function translateRequest (request, loadendFunction) { // 번역 요청
+	if (!getTkk()) {
+		tkkRequest(translate)
+	} else {
+		translate()
+	}
+
+	function translate () {
+		request = request.trim()
+		// console.log(request)
+		// console.log(encodeURIComponent(request))
+		let response = ''
+		let message = ''
+		let xhr = new XMLHttpRequest()
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				response = xhr.responseText
+				message = response
+				if (responseJSONArray.unshift(JSON.parse(response)) > 5) {
+					responseJSONArray.pop()
+				}
+				// console.log(response)
+				// console.log(JSON.parse(response))
+				console.log(responseJSONArray)
+			} else {
+				message = 'error'
+			}
+		}
+		xhr.open('POST', `https://translate.google.com/translate_a/single?client=webapp&sl=auto&tl=ko&hl=ko&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&otf=1&ssel=0&tsel=0&kc=1&tk=${getTk(request)}`, true)
+		xhr.setRequestHeader(requestHeader[0], requestHeader[1])
+		xhr.send(`q=${encodeURIComponent(request)}`)
+		xhr.onloadend = () => {
+			browser.notifications.create(
+				'notification id1',
+				{
+					type: 'basic',
+					title: 'translate',
+					message: message
+				}
+			)
+			if (loadendFunction) loadendFunction()
+		}
+	}
 }
--- project/Fn-Translate/script-content.js
+++ project/Fn-Translate/script-content.js
@@ -1,46 +1,38 @@
 const name1 = 'fn-translate'
-let x = 0
-let y = 0
+let xValue = 0
+let yValue = 0;
 
-$(() => { // 자동시작
+(() => {
 	// alert('test')
-	$(document).on(`mouseup.${name1}-button`, (e) => { // 마우스업 이벤트 추가
-		// console.log(e)
-		setTimeout(() => { // (setTimeout) 선택된 택스트 클릭하면 선택이 풀려도 버튼 재생성 방지
-			if (!$(e.target).parents(`.${name1}`).length) {
-				$(`.${name1}`).remove() // 버튼 제거
-				let selectionString = selectionStringGet()
-				if (selectionString) {
-					$('html body').append(htmlbutton()) // 버튼 추가
-					x = e.clientX
-					y = e.clientY
-					let wiwidth = window.innerWidth - 40
-					let wiheight = window.innerHeight - 40
-					if (x > wiwidth) x = wiwidth
-					if (y > wiheight) y = wiheight
-					if (x < 0) x = 0
-					if (y < 0) y = 0
-					$(`#${name1}-button`).css('left', x + 10) // 마우스 좌표로 이동
-					$(`#${name1}-button`).css('top', y + 10)
-					$(`#${name1}-button`).click((e) => { // 버튼 클릭 이벤트 추가
-						e.preventDefault()
-						translate(selectionString)
-						// console.log('button click')
-					})
+	document.addEventListener('mouseup', (event) => { // 마우스업 이벤트 추가
+		// console.log(event)
+		if (event.button === 0) { // 0: 마우스 왼쪽 버튼
+			setTimeout(() => { // setTimeout: 선택된 택스트 클릭하면 선택이 풀려도 버튼 재생성 방지
+				let target = event.target
+				while (target) { // 상위엘리먼트 탐색
+					// console.log(target)
+					if (target.classList.contains(name1)) { // 클래스 포함 여부
+						break
+					} else {
+						target = target.parentElement
+					}
 				}
-			}
-		}, 0)
-		// console.log('mouseup')
+				if (!target) {
+					translateButton(event)
+				}
+			}, 0)
+			// console.log('mouseup')
+		}
 	})
-})
+})()
 
-browser.runtime.onMessage.addListener((message) => { // 메시지 수신
-	if (message[0] === 'translate') {
-		translate(selectionStringGet())
+browser.runtime.onMessage.addListener((message) => { // 메시지 리스너
+	if (message.id === 'translate') {
+		translate(getSelectionText())
 	}
 })
 
-function selectionStringGet () {
+function getSelectionText () {
 	let selectString = document.getSelection().toString() // 선택된 텍스트 추출
 	if (selectString) {
 		// console.log(selectString)
@@ -55,66 +47,145 @@ function selectionStringGet () {
 	}
 }
 
-function translate (selectionString) {
-	if (!selectionString) return
+function translateButton (event) {
+	let element = document.getElementsByClassName(name1)[0]
+	if (element) element.remove() // 버튼 or 창 제거
+	let selectionText = getSelectionText()
+	if (selectionText) {
+		document.body.insertAdjacentHTML('beforeend', htmlBase())
+		document.getElementsByClassName(name1)[0].insertAdjacentHTML('beforeend', htmlButton()) // 버튼 추가
+		xValue = event.clientX
+		yValue = event.clientY
+		element = document.getElementById(`${name1}-button`)
+		moveElement(element)
+		element.addEventListener('click', (event) => { // 버튼 클릭 이벤트 추가
+			event.preventDefault()
+			// translate(selectionText)
+			browser.runtime.sendMessage({
+				id: 'translate',
+				selectionText: selectionText
+			}).then((response) => {
+				console.log(response)
+			})
+			// console.log('button click')
+		})
+	}
+}
+
+function translate (selectionText) {
+	if (!selectionText) return
 	let resultData = ''
-	$(`.${name1}`).remove()
-	$('html body').append(htmlpopup()) // 팝업 레이어 추가
-	let wiwidth = window.innerWidth - 400
-	let wiheight = window.innerHeight - 300
-	if (x > wiwidth) x = wiwidth
-	if (y > wiheight) y = wiheight
-	if (x < 0) x = 0
-	if (y < 0) y = 0
-	$(`#${name1}-page`).css('left', x + 10) // 마우스 좌표로 이동
-	$(`#${name1}-page`).css('top', y + 10)
-	$(`#${name1}-data1`).text(selectionString)
-	$(`#${name1}-page`).draggable({ scroll: false, cancel: `#${name1}-data1` }) // 드래그 기능
-	$.ajax({
-		type: 'POST',
-		url: 'http://192.168.99.100:5000/googletranslate',
-		data: { data: selectionString },
-		async: false, // 응답 대기
-		success: (response) => {
-			resultData = response
+	let button = document.getElementById(`${name1}-button`)
+	if (button) button.remove() // 버튼 제거
+	document.getElementsByClassName(name1)[0].insertAdjacentHTML('beforeend', htmlPopup()) // 팝업 레이어 추가
+	let page = document.getElementById(`${name1}-page`)
+	document.getElementById(`${name1}-data`).textContent = selectionText
+	moveElement(page)
+	let xhr = new XMLHttpRequest()
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			// console.log(xhr.responseText)
+			// console.log(JSON.parse(xhr.responseText))
+			resultData = JSON.parse(xhr.responseText)
 		}
-	})
+	}
+	xhr.open('POST', 'http://192.168.99.100:5000/googletranslate', false) // false: 동기
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+	xhr.send(`data=${encodeURIComponent(selectionText)}`)
 	// console.log(resultData)
 	// console.log(resultData.data1)
-	if (resultData.data2) { // 팝업 데이터 추가
+	let data = document.getElementById(`${name1}-data`)
+	if (resultData.data2) { // 팝업 데이터 갱신
 		// console.log(resultData.data2)
-		$(`#${name1}-data1`).text(`${resultData.data1}\n\n${resultData.data2}`)
+		data.textContent = `${resultData.data1}\n\n${resultData.data2}`
 	} else {
-		$(`#${name1}-data1`).text(resultData.data1)
+		data.textContent = resultData.data1
 	}
+	moveElement(page)
+	dragMoveElement(page, null, document.getElementById(`${name1}-data`)) // 드래그 기능 추가
 	// console.log('popup')
 }
 
-function htmlbutton () { // 팝업 생성 버튼
+function moveElement (element) { // 위치 조정
+	element.style.left = '0px'
+	element.style.top = '0px'
+	let xMove = xValue
+	let yMove = yValue
+	let wiwidth = window.innerWidth - (element.offsetWidth + 36) // +16 스크롤바 폭
+	let wiheight = window.innerHeight - (element.offsetHeight + 20)
+	if (xMove > wiwidth) xMove = wiwidth // 화면 이탈 방지
+	if (yMove > wiheight) yMove = wiheight
+	if (xMove < 0) xMove = 0
+	if (yMove < 0) yMove = 0
+	element.style.left = `${xMove + 10}px` // 마우스 좌표로 이동
+	element.style.top = `${yMove + 10}px`
+}
+
+function dragMoveElement (movingElement, targetElement, stopElement) { // 드래그 이동
+	let x1 = 0
+	let y1 = 0
+	if (targetElement) {
+		targetElement.addEventListener('mousedown', dragMouseDown)
+	} else {
+		movingElement.addEventListener('mousedown', dragMouseDown)
+	}
+	if (stopElement) {
+		stopElement.addEventListener('mousedown', (event) => {
+			event.stopPropagation() // 이벤트 전파 차단
+		})
+	}
+
+	function dragMouseDown (event) { // 드래그 시작
+		// event = event || window.event
+		event.preventDefault()
+		x1 = event.clientX
+		y1 = event.clientY
+		document.addEventListener('mouseup', stopDrag)
+		document.addEventListener('mousemove', elementDrag)
+	}
+
+	function stopDrag () { // 드래그 중지
+		document.removeEventListener('mouseup', stopDrag)
+		document.removeEventListener('mousemove', elementDrag)
+	}
+
+	function elementDrag (event) { // 드래그
+		// event = event || window.event
+		event.preventDefault()
+		let x2 = x1 - event.clientX
+		let y2 = y1 - event.clientY
+		x1 = event.clientX
+		y1 = event.clientY
+		movingElement.style.top = `${movingElement.offsetTop - y2}px`
+		movingElement.style.left = `${movingElement.offsetLeft - x2}px`
+	}
+}
+
+function htmlBase (params) { // 팝업 기초
 	return `
 <div class="${name1}">
 	<style>
 		.${name1}, .${name1} * {all:initial; }
 		.${name1} style {display: none; }
 		.${name1} div {display: block; }
+		.${name1} pre {display: block; font-family: monospace; white-space: pre; margin: 1em 0; }
 	</style>
-	<div id="${name1}-button" style="max-width: 100px; max-height: 100px; background-color:rgba(255,255,255,0.5); border: 1px solid; border-radius: 10px; padding: 10px; position: fixed; z-index: 2147483647; top: 50%; left: 50%; overflow: auto; cursor: pointer; ">
-	</div>
 </div>
 `
 }
 
-function htmlpopup () { // 팝업 레이어
+function htmlButton () { // 팝업 생성 버튼
 	return `
-<div class="${name1}">
-	<style>
-		.${name1}, .${name1} * {all:initial; }
-		.${name1} style {display: none; }
-		.${name1} div {display: block; }
-		.${name1} pre {display: block; font-family: monospace !important; white-space: pre; margin: 1em 0; }
-	</style>
-	<div id="${name1}-page" style="background-color:rgba(255,255,255,0.9); border: 2px solid; border-radius: 20px; padding: 0px; position: fixed; z-index: 2147483647; top: 50%; left: 50%; cursor: move; ">
-		<pre id="${name1}-data1" style="white-space: pre-wrap; max-width: 400px; max-height: 300px; margin: 20px; overflow: auto; "></pre>
+<div id="${name1}-button" style="background-color:rgba(255,255,255,0.5); border: 1px solid; border-radius: 10px; padding: 10px; position: fixed; z-index: 2147483647; cursor: pointer; ">
+</div>
+`
+}
+
+function htmlPopup () { // 팝업 레이어
+	return `
+<div id="${name1}-page" style="background-color:rgba(255,255,255,0.9); border: 2px solid; border-radius: 10px; padding: 8px; position: fixed; z-index: 2147483646; cursor: move; ">
+	<div style="max-width: 400px; max-height: 300px; overflow: auto; ">
+		<pre id="${name1}-data" style="white-space: pre-wrap; margin: 0px; font-family: 돋움체 !important; "></pre>
 	</div>
 </div>
 `
