31b9ca508ede426457adc7dd660620a405b967c8
tmp20190820 <54312936+tmp20190820@users.noreply.github.com>
2020-12-21 12:06:04 +0900

    create addon

---
+++ project/Fn-PrivateWindow/script-background.js
@@ -0,0 +1,78 @@
+let targetWindowId = browser.windows.WINDOW_ID_NONE
+
+browser.menus.create({ // 컨텍스트 메뉴 생성
+	id: 'openInPrivateWindow',
+	title: 'Open in Private Window',
+	contexts: ['link']
+})
+let menu1 = browser.menus.create({
+	id: 'setTargetWindow',
+	title: 'Set Target Window',
+	contexts: ['page'],
+	visible: false
+})
+// browser.menus.create({
+// 	id: 't1',
+// 	title: 't1',
+// 	contexts: ['all']
+// })
+
+browser.menus.onShown.addListener((menuInfo, tabInfo) => { // 컨텍스트 메뉴 감지
+	console.log(menuInfo)
+	console.log(tabInfo)
+	if (tabInfo.incognito) {
+		browser.menus.update(menu1, {
+			visible: true
+		}).then(() => {
+			browser.menus.refresh()
+		})
+	} else {
+		browser.menus.update(menu1, {
+			visible: false
+		}).then(() => {
+			browser.menus.refresh()
+		})
+	}
+})
+
+browser.menus.onClicked.addListener((menuInfo, tabInfo) => { // 컨텍스트 메뉴 동작
+	// console.log(menuInfo)
+	// console.log(tabInfo)
+	switch (menuInfo.menuItemId) {
+	case 'openInPrivateWindow':
+		browser.windows.get(targetWindowId).then((windowInfo) => { // 타겟 윈도우 확인
+			// console.log(windowInfo)
+			browser.tabs.create({
+				active: false,
+				windowId: targetWindowId,
+				url: menuInfo.linkUrl
+			})
+		}, (errorMessage) => { // 타겟 윈도우가 없으면
+			// console.log(errorMessage)
+			browser.windows.create({
+				// focused: false // 작동 안함(v78.6.0esr) !
+				incognito: true,
+				url: menuInfo.linkUrl
+			}).then((windowInfo) => {
+				// console.log(windowInfo)
+				targetWindowId = windowInfo.id
+				browser.windows.update(tabInfo.windowId, {
+					focused: true
+				})
+				// .then((windowInfo) => {
+				// 	// console.log(windowInfo)
+				// }, (errorMessage) => {
+				// 	// console.log(errorMessage)
+				// })
+			}, (errorMessage) => {
+				// console.log(errorMessage)
+			})
+		})
+		break
+	case 'setTargetWindow':
+		targetWindowId = tabInfo.windowId
+		break
+	// case 't1':
+	// 	break
+	}
+})
